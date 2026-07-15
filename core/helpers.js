31c9b7a9587f9829a07fbdaf9bf554b4f5c90223
Aklic <aklicstudio@gmail.com>
2026-07-15 11:05:23 +0530

    Update helpers.js

--- core/helpers.js
+++ core/helpers.js
@@ -1,1 +1,33 @@
+function $(selector) {
 
+    return document.querySelector(selector);
+
+}
+
+function $$(selector) {
+
+    return document.querySelectorAll(selector);
+
+}
+
+function createElement(tag, className = "") {
+
+    const element = document.createElement(tag);
+
+    if (className) {
+
+        element.className = className;
+
+    }
+
+    return element;
+
+}
+
+function loadHTML(path) {
+
+    return fetch(path)
+
+        .then(response => response.text());
+
+}
