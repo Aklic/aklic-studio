0c36d0de91d500c735f06bd9982426fdb1e0c822
Aklic <aklicstudio@gmail.com>
2026-07-15 11:32:56 +0530

    Update navigation.js

--- ui/layout/navigation/navigation.js
+++ ui/layout/navigation/navigation.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    loadHTML("ui/layout/navigation/navigation.html")
+
+    .then(html => {
+
+        const navigation =
+            document.getElementById("navigation");
+
+        if(navigation){
+
+            navigation.innerHTML = html;
+
+        }
+
+    });
+
+});
