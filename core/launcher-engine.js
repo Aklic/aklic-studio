aba014d0b3d30a5683f45187d5e69882f47d6db4
Aklic <aklicstudio@gmail.com>
2026-07-16 12:39:52 +0530

    Update launcher-engine.js

--- core/launcher-engine.js
+++ core/launcher-engine.js
@@ -1,1 +1,29 @@
+function connectLauncherButtons(){
 
+    const startButton =
+        document.getElementById("start-game-btn");
+
+    const fullscreenButton =
+        document.getElementById("fullscreen-btn");
+
+    if(startButton){
+
+        startButton.onclick = () => {
+
+            loadPlayer(window.currentGame);
+
+        };
+
+    }
+
+    if(fullscreenButton){
+
+        fullscreenButton.onclick = () => {
+
+            enterFullscreen();
+
+        };
+
+    }
+
+}
