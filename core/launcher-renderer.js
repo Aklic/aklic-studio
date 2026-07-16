9d04210996b456c3eb5c9be27ae36abbf9b6c679
Aklic <aklicstudio@gmail.com>
2026-07-16 12:39:23 +0530

    Update launcher-renderer.js

--- core/launcher-renderer.js
+++ core/launcher-renderer.js
@@ -1,1 +1,17 @@
+function fillLauncher(game){
 
+    document.getElementById("launcher-banner").src =
+        game.banner;
+
+    document.getElementById("launcher-title").textContent =
+        game.title;
+
+    document.getElementById("launcher-badge").textContent =
+        game.badge;
+
+    document.getElementById("launcher-description").textContent =
+        game.shortDescription;
+
+    connectLauncherButtons();
+
+}
