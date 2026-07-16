f2f589c6f9efdbf6c810429ac11420b6e0ad48d9
Aklic <aklicstudio@gmail.com>
2026-07-16 12:43:51 +0530

    Update game-loader.js

--- core/game-loader.js
+++ core/game-loader.js
@@ -1,1 +1,22 @@
+function loadGame(game){
 
+    const screen =
+        document.querySelector(".game-screen");
+
+    if(!screen) return;
+
+    screen.innerHTML = `
+
+<iframe
+    id="game-frame"
+    src="${game.gamePath}"
+    title="${game.title}"
+    allowfullscreen>
+
+</iframe>
+
+`;
+
+    connectExitButton();
+
+}
