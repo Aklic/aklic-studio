1a25254d027bbebc1a79ad9044d5c3db6c79bf8a
Aklic <aklicstudio@gmail.com>
2026-07-16 11:09:09 +0530

    Update footer.js

--- ui/layout/footer/footer.js
+++ ui/layout/footer/footer.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    loadHTML("ui/layout/footer/footer.html")
+
+    .then(html => {
+
+        const footer =
+            document.getElementById("footer");
+
+        if(footer){
+
+            footer.innerHTML = html;
+
+        }
+
+    });
+
+});
