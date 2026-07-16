370dcb0244ab6c86d76b7d9b1c75bf8cc7b014d0
Aklic <aklicstudio@gmail.com>
2026-07-16 11:15:23 +0530

    Update game-renderer.js

--- core/game-renderer.js
+++ core/game-renderer.js
@@ -1,1 +1,57 @@
+function createGameCard(game){
 
+    return `
+
+<article
+class="game-card"
+onclick="location.href='game.html?id=${game.id}'">
+
+    <div class="game-thumbnail">
+
+        <img
+        src="${game.thumbnail}"
+        alt="${game.title}">
+
+    </div>
+
+    <div class="game-content">
+
+        <h3 class="game-title">
+
+            ${game.title}
+
+        </h3>
+
+        <span class="play-link">
+
+            ▶ Play Game
+
+        </span>
+
+    </div>
+
+</article>
+
+`;
+
+}
+
+async function renderFeaturedGames(){
+
+    const games =
+    await getGames();
+
+    const grid =
+    document.getElementById(
+        "featured-games-grid"
+    );
+
+    if(!grid) return;
+
+    grid.innerHTML =
+    games
+    .filter(game=>game.status==="published")
+    .map(createGameCard)
+    .join("");
+
+}
